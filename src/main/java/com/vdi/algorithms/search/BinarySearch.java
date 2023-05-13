fd1d9f8f0d75bcc57c82a1717688c3f2e8b45afe
Vlad Dyusupov <vlad@light-tech.io>
2023-05-13 16:05:05 +0400

    binary search and tests for it

--- src/main/java/com/vdi/algorithms/search/BinarySearch.java
+++ src/main/java/com/vdi/algorithms/search/BinarySearch.java
@@ -1,5 +1,32 @@
 package com.vdi.algorithms.search;
 
-public class BinarySearch {
+import java.util.Arrays;
 
+public class BinarySearch<T extends Comparable<T>> extends Search<T> {
+
+    public BinarySearch(T[] orderedArray) {
+        super(orderedArray);
+    }
+
+    @Override
+    public int search(T element) {
+        int foundedElementIndex = -1;
+        int startIndex = 0;
+        int lastIndex = array.length - 1;
+        while (startIndex <= lastIndex) {
+            int midpoint = (lastIndex + startIndex) / 2;
+            int compareResult = element.compareTo(array[midpoint]);
+
+            if (compareResult == 0) {
+                foundedElementIndex = midpoint;
+                break;
+            } else if (compareResult < 0) {
+                lastIndex = midpoint - 1;
+            } else {
+                startIndex = midpoint + 1;
+            }
+        }
+
+        return foundedElementIndex;
+    }
 }
--- src/main/test/com/vdi/algorithms/search/SearchTest.java
+++ src/main/test/com/vdi/algorithms/search/SearchTest.java
@@ -17,6 +17,9 @@ class SearchTest {
     static List<Search<Integer>> integerSearchAlgorithms = new ArrayList<>();
     static List<Search<String>> stringSearchAlgorithms = new ArrayList<>();
     static List<Search<Fruit>> objectSearchAlgorithms = new ArrayList<>();
+    private static Integer[] testIntegers;
+    private static String[] testStrings;
+    private static Fruit[] testCustomObjects;
 
     @BeforeAll
     static void init() {
@@ -26,21 +29,29 @@ class SearchTest {
     }
 
     private static void initIntegerSearchers() {
-        LinearSearch<Integer> integerLinearSearch = new LinearSearch<>(new Integer[]{1, 2, 3, 4, 5});
+        testIntegers = new Integer[]{1, 120, 260, 340, 550, 731, 1231};
+        Search<Integer> integerLinearSearch = new LinearSearch<>(testIntegers);
+        Search<Integer> integerBinarySearch = new BinarySearch<>(testIntegers);
         integerSearchAlgorithms.add(integerLinearSearch);
+        integerSearchAlgorithms.add(integerBinarySearch);
     }
+
     private static void initStringSearchers() {
-        LinearSearch<String> stringLinearSearch = new LinearSearch<>(new String[]{
+        testStrings = new String[]{
                 "apple", "banana", "orange", "kiwifruit", "coconut"
-        });
+        };
+        Search<String> stringLinearSearch = new LinearSearch<>(testStrings);
         stringSearchAlgorithms.add(stringLinearSearch);
     }
+
     private static void initCustomObjectSearchers() {
-        LinearSearch<Fruit> objectLinearSearch = new LinearSearch<>(new Fruit[]{
+        testCustomObjects = new Fruit[]{
                 new Fruit("apple"), new Fruit("banana"), new Fruit("orange"), new Fruit("kiwifruit"), new Fruit("coconut")
-        });
+        };
+        Search<Fruit> objectLinearSearch = new LinearSearch<>(testCustomObjects);
         objectSearchAlgorithms.add(objectLinearSearch);
     }
+
     static class Fruit {
         private final String name;
 
@@ -64,29 +75,32 @@ class SearchTest {
 
 
     @Test
+    void search_shouldReturnCorrectInteger() {
+        for (Integer integerToFind : testIntegers) {
+            for (Search<Integer> searchAlgorithm : integerSearchAlgorithms) {
+                Integer foundedElement = searchAlgorithm.get(integerToFind);
+                assertEquals(integerToFind, foundedElement, searchAlgorithm.toString().concat(", doesn't return correct element"));
+            }
+        }
+    }
+
+    @Test
     void search_shouldReturnCorrectString() {
-        String elementToFind = "orange";
-        for (Search<String> searchAlgorithm : stringSearchAlgorithms) {
-            String foundedElement = searchAlgorithm.get(elementToFind);
-            assertEquals(elementToFind, foundedElement);
+        for (String stringToFind : testStrings) {
+            for (Search<String> searchAlgorithm : stringSearchAlgorithms) {
+                String foundedElement = searchAlgorithm.get(stringToFind);
+                assertEquals(stringToFind, foundedElement);
+            }
         }
     }
 
     @Test
     void search_shouldReturnCorrectObject() {
-        Fruit elementToFind = new Fruit("orange");
-        for (Search<Fruit> searchAlgorithm : objectSearchAlgorithms) {
-            Fruit foundedElement = searchAlgorithm.get(elementToFind);
-            assertEquals(elementToFind, foundedElement);
-        }
-    }
-
-    @Test
-    void search_shouldReturnCorrectInteger() {
-        Integer elementToFind = 3;
-        for (Search<Integer> searchAlgorithm : integerSearchAlgorithms) {
-            Integer foundedElement = searchAlgorithm.get(elementToFind);
-            assertEquals(elementToFind, foundedElement);
+        for (Fruit objectToFind : testCustomObjects) {
+            for (Search<Fruit> searchAlgorithm : objectSearchAlgorithms) {
+                Fruit foundedElement = searchAlgorithm.get(objectToFind);
+                assertEquals(objectToFind, foundedElement);
+            }
         }
     }
 
